0fbfca9e52cb5bdabc15950b8b056df36d7ffbd2
Aniket Pratap <PratapA1028@sduhsd.lan>
2019-02-01 11:11:41 -0800

    unit 01

---
+++ Unit01/src/StarsRunner.java
@@ -0,0 +1,24 @@
+//© A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class StarsRunner
+{
+   public static void main(String args[])
+   {
+      //instantiate a StarsAndStripes object
+	   StarsAndStripes flag = new StarsAndStripes();
+      
+      //call the methods needed to make the patterns on the word document
+	   flag.printASmallBox();
+	   flag.printTwoBlankLines();
+	   flag.printABigBox();
+      
+   }
+}
