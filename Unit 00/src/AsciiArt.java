7805d0901c24f610aa34615f4e89c8f2714b6ad4
Aniket Pratap <PratapA1028@sduhsd.lan>
2019-02-01 10:23:36 -0800

    added files for unit00

---
+++ Unit 00/src/AsciiArt.java
@@ -0,0 +1,39 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class AsciiArt
+{
+	public static void main ( String[] args )
+	{
+		System.out.println("Aniket Pratap \n\n" );
+		System.out.println("Lion" );
+		System.out.println("\n\n\n\n" );
+
+		System.out.println("###################" );
+		
+		
+		for(int i = 0; i < 2; i++) {
+			System.out.println("|##             ##|" );
+		}
+		
+		System.out.println("|##   O    O    ##|" );
+		System.out.println("|##   > <> <    ##|");
+		
+		
+		for(int i = 0; i < 1; i++) {
+			System.out.println("|##    WWWWW    ##|" );
+		}
+		System.out.println("###################" );
+		//add other output
+
+		System.out.println(" \n\n\n\nHelpFul Hints" );
+		System.out.println("\\\\ draws one backslash on the screen!\n" );
+		System.out.println("\\\" draws one double quote on the screen!\n" );
+		System.out.println("\\\' draws one single quote on the screen!\n" ); 
+	}
+}
---
+++ Unit 00/src/AsciiBox.java
@@ -0,0 +1,53 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class AsciiBox
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Aniket Pratap \t  1/31/19 \n\n" );
+		
+		for(int i = 0; i < 3; i++) 
+			{
+		System.out.println("+++++++++++++++++++++++++ " );
+			}
+		
+		for(int x = 0; x < 2; x++) 
+			{
+			System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA");
+			}
+		
+		for(int i = 0; i < 3; i++) 
+			{
+			System.out.println("+++++++++++++++++++++++++ " );
+			}
+			
+		for(int x = 0; x < 2; x++) 
+			{
+			System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA");
+			}
+		
+		for(int i = 0; i < 3; i++) 
+			{
+			System.out.println("+++++++++++++++++++++++++ " );
+			}
+				
+		for(int x = 0; x < 2; x++) 
+			{
+			System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA");
+			}
+		
+		for(int i = 0; i < 3; i++) 
+			{
+			System.out.println("+++++++++++++++++++++++++ " );
+			}
+		
+	}
+	
+}
+	
