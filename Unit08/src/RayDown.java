434eba4323445f1448f1d17069a64a24a7168fc6
xXViridianXx <43630346+xXViridianXx@users.noreply.github.com>
2019-02-27 19:20:31 -0800

    update

--- Unit08/src/RayDown.java
+++ Unit08/src/RayDown.java
@@ -9,7 +9,7 @@ public class RayDown
 	//are in decreasing order [31,12,6,2,1]
 	public static boolean go(int[] numArray)
 	{
-		boolean decreasing = false;
+		
 		
 		for (int i = 0; i < numArray.length -1; i++)
 		{
