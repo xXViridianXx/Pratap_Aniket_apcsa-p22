57a6685cc9b0b6849bc5837927f8668bfbac5501
Aniket Pratap <PratapA1028@sduhsd.lan>
2019-03-28 11:12:14 -0700

    vklcnbiofg

--- unit14/src/Grid.java
+++ unit14/src/Grid.java
@@ -11,20 +11,54 @@ public class Grid
 	public Grid(int rows, int cols, String[] vals)
 	{
 		
-	
+		grid = new String[rows][cols];
+		
+		for (int i = 0; i < rows; i++)
+			
+			for(int j = 0; j < cols; j++)
+			{
+				grid[i][j] = vals[(int) Math.random() * vals.length];
+			}
+		
+		
+		values = vals;
 		
 	}
 
 	//find out which of the vals occurs the most
 	public String findMax(String[] vals)
 	{
-		return "nothing yet";
+		String max = vals[0];
+		
+		for(String v : vals)
+		{
+			if(countVals(v) > countVals(max))
+			{
+				max = v;
+			}
+		}
+		
+		return max;
 	}
 
 	//returns a count of how many times val occurs in the matrix
 	private int countVals( String val )
 	{
-		return 0;
+		int count = 0;
+		
+		for(String[] i : grid)
+		{
+			for(String j:i)
+			{
+				if(j.contentEquals(val))
+				{
+					count++;
+				}
+				
+			}
+		}
+		
+		return count;
 	}
 
 	//display the grid
