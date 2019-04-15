59ac0d67f370965c1799c76731340d9335226465
Aniket Pratap <PratapA1028@sduhsd.lan>
2019-04-15 11:05:14 -0700

    1

---
+++ UUUnit15/src/Paddle.java
@@ -0,0 +1,52 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Paddle extends Block
+{
+   //instance variables
+   private int speed;
+
+   public Paddle()
+   {
+		super(10,10);
+      speed =5;
+   }
+
+
+   //add the other Paddle constructors
+   
+   
+public Paddle(int x, int y)
+{
+	
+}
+
+
+
+
+
+
+
+
+
+   public void moveUpAndDraw(Graphics window)
+   {
+
+
+   }
+
+   public void moveDownAndDraw(Graphics window)
+   {
+
+
+   }
+
+   //add get methods
+   
+   
+   //add a toString() method
+}
