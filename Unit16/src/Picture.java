a80bc4f691edaeb28feeca6f534f5569b7e60c72
Aniket Pratap <PratapA1028@sduhsd.lan>
2019-04-22 11:11:25 -0700

    rerete

--- Unit16/src/Picture.java
+++ Unit16/src/Picture.java
@@ -98,6 +98,60 @@ public class Picture extends SimplePicture
     }
   }
   
+  public void keepOnlyBlue()
+  {
+	  Pixel[][] pixels = this.getPixels2D();
+	    for (Pixel[] rowArray : pixels)
+	    {
+	      for (Pixel pixelObj : rowArray)
+	      {
+	        pixelObj.setRed(0);
+	        pixelObj.setGreen(0);
+	        
+	      }
+	    }
+	  }
+  
+  public void negate()
+  {
+	  Pixel[][] pixels = this.getPixels2D();
+	    for (Pixel[] rowArray : pixels)
+	    {
+	      for (Pixel pixelObj : rowArray)
+	      {
+	        pixelObj.setRed(255 - pixelObj.getRed());
+	        pixelObj.setGreen(255 -  pixelObj.getGreen() );
+	        pixelObj.setBlue(255 - pixelObj.getBlue());
+	        
+	      }
+	    }
+  }
+  public void  grayscale()
+  {
+	  
+	  
+	  
+	  Pixel[][] pixels = this.getPixels2D();
+	    for (Pixel[] rowArray : pixels)
+	    {
+	      for (Pixel pixelObj : rowArray)
+	      {
+	    	 
+	    	  int avg = (int) pixelObj.getAverage();
+		        pixelObj.setBlue(avg);
+		        pixelObj.setRed(avg);
+		        pixelObj.setGreen(avg);
+	    	  
+	      }
+	    }
+  }
+  
+  public void fixUnderwater()
+  {
+	  
+  }
+  
+  
   /** Method that mirrors the picture around a 
     * vertical mirror in the center of the picture
     * from left to right */
